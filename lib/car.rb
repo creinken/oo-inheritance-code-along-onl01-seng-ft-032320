e9fd3909486b1961f2577e268c7663ff3c77a1d3
Christopher <reinkendigital@gmail.com>
2020-04-07 12:29:02 -0500

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,14 @@
 require_relative "./vehicle.rb"
+
+class Car < Vehicle
+
+    #### Attributes ####
+
+    #### Instance Methods ####
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+
+    #### Class Methods####
+
+end
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,22 @@
 class Vehicle
 
+    #### Attributes ####
+    attr_accessor :wheel_size, :wheel_number
+
+    #### Instance Methods ####
+    def initialize(wheel_size, wheel_number)
+        @wheel_size = wheel_size
+        @wheel_number = wheel_number
+    end
+
+    def go
+        "vrrrrrrrooom!"
+    end
+
+    def fill_up_tank
+        "filling up!"
+    end
+
+    #### Class Methods####
+
 end
